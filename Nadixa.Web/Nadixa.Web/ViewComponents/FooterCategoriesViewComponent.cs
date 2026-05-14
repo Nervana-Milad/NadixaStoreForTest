d7deb5ee3e5a23207f2fa9dcbada3350fd292e0c
Nervana-Milad <101606226+Nervana-Milad@users.noreply.github.com>
2026-05-14 14:25:25 +0300

    Added ProductCategories component

---
+++ Nadixa.Web/Nadixa.Web/ViewComponents/FooterCategoriesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nadixa.Infrastructure.Data;
+
+namespace Nadixa.Web.ViewComponents
+{
+    public class FooterCategoriesViewComponent : ViewComponent
+    {
+        private readonly NadixaDbContext _context;
+
+        public FooterCategoriesViewComponent(NadixaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.ProductCategories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
