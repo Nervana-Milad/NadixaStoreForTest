ded538f312e484f101c7a13b2f19f4930eccf297
Nervana-Milad <101606226+Nervana-Milad@users.noreply.github.com>
2026-05-21 15:22:56 +0300

    added view component for category menu

---
+++ Nadixa.Web/Nadixa.Web/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nadixa.Infrastructure.Data;
+
+namespace Nadixa.Web.ViewComponents
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly NadixaDbContext _context;
+
+        public CategoryMenuViewComponent(NadixaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.ProductCategories
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
