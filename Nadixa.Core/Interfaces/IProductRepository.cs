4e33ccca0bab5f3a5adfc37d80c5cda31f2e88ce
Nermenhosny <nermenhosny9@gmail.com>
2026-07-21 13:26:40 +0300

    Seperation of Concerns of Product

---
+++ Nadixa.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Nadixa.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nadixa.Core.Interfaces
+{
+    public interface IProductRepository
+        {
+            Task<(List<Product> Items, int TotalCount)> GetPagedAsync(
+                int? categoryId, int? subCategoryId, string? search, int page, int pageSize);
+        }
+    
+}
---
+++ Nadixa.Infrastructure/Nadixa.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nadixa.Core.Entities;
+using Nadixa.Core.Interfaces;
+using Nadixa.Infrastructure.Data;
+
+namespace Nadixa.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly NadixaDbContext _context;
+
+        public ProductRepository(NadixaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<(List<Product> Items, int TotalCount)> GetPagedAsync(
+            int? categoryId, int? subCategoryId, string? search, int page, int pageSize)
+        {
+            IQueryable<Product> query = _context.Products
+                .AsNoTracking()
+                .Include(p => p.ProductCategory);
+
+            if (categoryId.HasValue)
+                query = query.Where(p => p.ProductCategoryId == categoryId.Value);
+
+            if (subCategoryId.HasValue)
+                query = query.Where(p => p.ProductSubCategoryId == subCategoryId.Value);
+
+            if (!string.IsNullOrWhiteSpace(search))
+                query = query.Where(p => p.Name.Contains(search) || p.Description.Contains(search));
+
+            int total = await query.CountAsync();
+
+            var items = await query
+                .OrderByDescending(p => p.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (items, total);
+        }
+    }
+}
